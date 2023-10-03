2d1706633e3f925515cf5795e64c4c55cb85ab9a
annazlwagner <115283284+annazlwagner@users.noreply.github.com>
2023-10-03 15:44:36 -0400

    edit code: fetch(functionApiUrl)

--- frontend/main.js
+++ frontend/main.js
@@ -3,10 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 }) //when the content is loaded, go & run this event - getVisitCount
 
+const functionApiUrl = ''
+//^this the url from the function app, function in Azure portal
+
 // declare a constant:
 //within the '', we put the URL to the API. We'll have tht 
 //later when we implement the API via an Azure function
-const functionApi = '';
+const localfunctionApi = 'http://localhost:7071/api/GetCounter';
+//^^^added the url from running the function in api folder (func host start)
 
 //this code will grab the JSON that is provided from that API -
 //grabbing the correct part of tht JSON & showing it in our HTML
@@ -16,7 +20,7 @@ const getVisitCount = () => {
     //^declare a variable that holds count data. Can be whatever # bc it will be changed below
 
     
-    fetch(functionApi).then(response => { //fetch the data from that URL that we're providing in functionApi 
+    fetch(functionApiUrl).then(response => { //fetch the data from that URL that we're providing in functionApi 
         return response.json() //then grab the response & return the response.JSON
     }).then(response =>{ //grab the response
         console.log("Website called function API :D"); //log a message to the console for debugging purposes
